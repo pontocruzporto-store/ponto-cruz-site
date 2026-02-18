42d8d3dfb43f2c6f549c2d525b4431b6b4c9a9ae
pontocruzporto-store <pontocruz.porto@gmail.com>
2026-02-18 15:26:47 +0000

    Improvements in LCP

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     emptyOutDir: true,
     rollupOptions: {
       output: {
-        manualChunks: undefined
-      }
-    }
+        manualChunks: undefined,
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === "style.css") {
+            return "assets/critical.css";
+          }
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+    cssCodeSplit: true,
   },
-  base: '/'
+  base: "/",
 });
